3efff1b7e41ff42aa2691ec07a7fb48e4cec7ae9
Diogo Oliveira <ddiogooliveira9@gmail.com>
2021-12-11 11:49:44 -0300

    Utilizando os métodos HTTP

--- src/index.js
+++ src/index.js
@@ -2,8 +2,29 @@ const express = require('express')
 
 const app = express()
 
-app.get("/", (request, response) => {
-    return response.json({ message: "Hello World Ignite!" })
+// GET - Buscar uma informação dentro do servidor
+app.get("/courses", (request, response) => {
+    return response.json([ "Curso 1", "Curso 2", "Curso 3"])
+})
+
+// POST - Inserir uma informação no servidor
+app.post("/courses", (request, response) => {
+    return response.json([ "Curso 1", "Curso 2", "Curso 3", "Curso 4"])
+})
+
+// PUT - Alterar uma informação no servidor
+app.put("/courses/:id", (request, response) => {
+    return response.json([ "Curso 6", "Curso 2", "Curso 3", "Curso 4"])
+})
+
+// PATCH - Alterar uma informação específica
+app.patch("courses/:id", (request, response) => {
+    return response.json([ "Curso 6", "Curso 7", "Curso 3", "Curso 4"])
+})
+
+// DELETE - Deletar uma informação no servidor
+app.delete("courses/:id", (request, response) => {
+    return response.json([ "Curso 6", "Curso 7", "Curso 4"])
 })
 
 app.listen(3333)
